6bed5406d40105232b03016aa275d7405126947b
NelinVitaliy <s5001481.nelin@gmail.com>
2019-12-30 01:54:14 +0600

    MolchanovProject

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nelin/NeLiN/MakerS Python/Lecture/Week_7/MolchanovProject/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
